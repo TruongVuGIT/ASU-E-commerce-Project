52591a74f39b6ae1bc7c4ffbeabfdf0d17025306
Unknown <djboldt@asu.edu>
2017-02-17 13:47:43 -0700

    Added order.xml, added Kenji's "Add to Cart" function into carts.cs, and added <order> to data.xml.

--- ASU_E-Commerce_Web_Services/ASU_E-Commerce_Web_Services/ASU_E-Commerce_Web_Services/carts.cs
+++ ASU_E-Commerce_Web_Services/ASU_E-Commerce_Web_Services/ASU_E-Commerce_Web_Services/carts.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace ASU_E_Commerce_Web_Services
 {
     public class carts
     {
+        public string add_cart(string userid, string productid, string quantity)
+        {
 
+            string file_location = AppDomain.CurrentDomain.BaseDirectory + @"/carts.xml";
+            XElement xml = XElement.Load(file_location);
+            xml.Add(new XElement("cart", new XAttribute("userid", userid),
+                new XElement("productid", new XAttribute("quantity", quantity), productid)));
+            xml.Save(file_location);
+
+            return "pass";
+        }
     }
 }
